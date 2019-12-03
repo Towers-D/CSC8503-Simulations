c9d2cb58690bb29af24e7095913345981f58d6d3
David Andrew Towers <d.towers2@ncl.ac.uk>
2019-12-03 10:52:40 +0000

    Implemented Broadphase and Narrow Phase

--- CSC8503/CSC8503Common/PhysicsSystem.cpp
+++ CSC8503/CSC8503Common/PhysicsSystem.cpp
@@ -171,7 +171,7 @@ void PhysicsSystem::BasicCollisionDetection() {
 				continue;
 			CollisionDetection::CollisionInfo info;
 			if (CollisionDetection::ObjectIntersection(*i, *j, info)) {
-				std::cout << "Collision between " << (*i)->GetName() << " and " << (*j)->GetName() << std::endl;
+				//std::cout << "Collision between " << (*i)->GetName() << " and " << (*j)->GetName() << std::endl;
 				ImpulseResolveCollision(*info.a, *info.b, info.point);
 				info.framesLeft = numCollisionFrames;
 				allCollisions.insert(info);
@@ -239,7 +239,28 @@ compare the collisions that we absolutely need to.
 */
 
 void PhysicsSystem::BroadPhase() {
+	broadphaseCollisions.clear();
+	QuadTree<GameObject*> tree(Vector2(1024, 1024), 7, 6);
 
+	std::vector<GameObject*>::const_iterator first;
+	std::vector<GameObject*>::const_iterator last;
+	gameWorld.GetObjectIterators(first, last);
+	for (auto i = first; i != last; ++i) {
+		Vector3 halfSizes;
+		if (!(*i)->GetBroadphaseAABB(halfSizes))
+			continue;
+		Vector3 pos = (*i)->GetConstTransform().GetWorldPosition();
+		tree.Insert(*i, pos, halfSizes);
+	}
+	tree.OperateOnContents([&](std::list<QuadTreeEntry<GameObject*>>& data) {
+		CollisionDetection::CollisionInfo info;
+		for (auto i = data.begin(); i != data.end(); ++i)
+				for(auto j = std::next(i); j != data.end(); ++j) {
+				info.a = min((*i).object, (*j).object);
+				info.b = max((*i).object, (*j).object);
+				broadphaseCollisions.insert(info);
+			}
+	});
 }
 
 /*
@@ -248,7 +269,14 @@ The broadphase will now only give us likely collisions, so we can now go through
 and work out if they are truly colliding, and if so, add them into the main collision list
 */
 void PhysicsSystem::NarrowPhase() {
-
+	for (std::set<CollisionDetection::CollisionInfo>::iterator i = broadphaseCollisions.begin(); i != broadphaseCollisions.end(); ++i) {
+		CollisionDetection::CollisionInfo info = *i;
+		if (CollisionDetection::ObjectIntersection(info.a, info.b, info)) {
+			info.framesLeft = numCollisionFrames;
+			ImpulseResolveCollision(*info.a, *info.b, info.point);
+			allCollisions.insert(info);
+		}
+	}
 }
 
 /*
